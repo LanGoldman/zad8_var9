07db4cec68eb00cbd764b6abec25729dffa35d4f
Your Name <you@example.com>
2023-02-13 00:21:30 +0500

    zad8

---
+++ app/src/main/java/com/example/zad8_var9/collection_screen.java
@@ -0,0 +1,6 @@
+package com.example.zad8_var9;
+
+import android.app.Activity;
+
+public class collection_screen extends Activity {
+}
---
+++ app/src/main/java/com/example/zad8_var9/film_list.java
@@ -0,0 +1,6 @@
+package com.example.zad8_var9;
+
+import android.app.Activity;
+
+public class film_list extends Activity {
+}
